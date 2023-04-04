0e39ffd7a0d863491786b9ad40008b7442cb35aa
marco.rainone <marco.rainone@eccenture.com>
2023-04-04 16:43:41 +0200

    first commit

---
+++ src/main/java/com/training/demo/controller/DemoApplicationController.java
@@ -0,0 +1,62 @@
+package com.training.demo.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.training.demo.model.Studente;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+public class DemoApplicationController {
+
+
+    /* Invocazione: http://localhost:8080/hello/marco */
+
+    @GetMapping(path="/hello/{name}")
+    public String hello(@PathVariable String name)
+        {
+            return "Hello" + name;
+        }
+    @GetMapping(path="/hello")
+    public String helloDue(@RequestParam String name)
+    {
+        return "Hello" + name;
+    }
+
+    @RequestMapping(value = "/helloMyName", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity helloMyName(HttpServletRequest req, @RequestParam String name,@RequestParam String biname) {
+        String concat;
+        try {
+            concat = name + " " + biname;
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+        return ResponseEntity.ok(concat);
+    }
+
+    @RequestMapping(value = "/postMyName", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity postMyNameMarco(HttpServletRequest req, @RequestBody String objectRequest) {
+
+        Studente nuovoStudente = new Studente();
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            nuovoStudente = mapper.readValue(objectRequest, new TypeReference<Studente>(){});
+
+            nuovoStudente.setBiname("Ognibene");
+
+
+        } catch (JsonProcessingException e) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+        return ResponseEntity.ok(nuovoStudente);
+    }
+
+
+
+}
---
+++ src/main/java/com/training/demo/model/Studente.java
@@ -0,0 +1,26 @@
+package com.training.demo.model;
+
+public class Studente {
+    private String name;
+    private String biname;
+
+    public Studente(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiname() {
+        return biname;
+    }
+
+    public void setBiname(String biname) {
+        this.biname = biname;
+    }
+}
